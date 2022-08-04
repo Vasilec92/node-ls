d7e678fc973adf7d593213c8eb9d39832874c001
Anna Vasileva <vasilec1992@yandex.rum>
2022-08-04 15:36:40 +0200

    dz

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 // ● второе — жёлтым;
 // ● третье — красным.
 
-const colors = require("colors/safe");
+/* const colors = require("colors/safe");
 let readline = require("readline");
 
 const isPrimeNum = (num) => {
@@ -46,4 +46,46 @@ rl.on("line", (input) => {
   }
 
   //rl.close();
-});
+}); */
+let rlSync = require("readline-sync");
+const colors = require("colors/safe");
+
+const isPrimeNum = (num) => {
+  for (let i = 2; i < num; i++) {
+    if (num % i == 0) {
+      return false;
+    }
+  }
+  return true;
+};
+
+let from = rlSync.question("Diaposon from ?\n");
+let to = rlSync.question("Diaposon to ?\n");
+console.log(`Diaposon, ${from}-${to}`);
+
+for (let i = Number(from); i <= Number(to); i++) {
+  console.log(i);
+}
+
+let col = "red";
+for (let i = 3; i <= to; i++) {
+  if (isPrimeNum(i)) {
+    if (col === "red") {
+      col = "green";
+      console.log(colors.green(i));
+      return;
+    }
+    if (col === "green") {
+      col = "yellow";
+      console.log(colors.yellow(i));
+      return;
+    }
+    if (col === "yellow") {
+      col = "red";
+      console.log(colors.red(i));
+      return;
+    }
+  } else {
+    console.log(colors.red(`${i}: Not prime number`));
+  }
+}
